460745369591f90650d08e81daefc3f1b03a0528
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-20 12:22:30 +0300

    vendor registration

--- routes/vendor/reg-log.js
+++ routes/vendor/reg-log.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { Register, Login,updateVendorProfile } = require("../../controllers/vendor/reg-log");
+const {
+  Register,
+  Login,
+  updateVendorProfile,
+} = require("../../controllers/vendor/reg-log");
+const VendorAuthMiddleware = require("../../middleware/vendormw");
 
 router.route("/register").post(Register);
 router.route("/login").post(Login);
-router.route('/updatevendorprofile/:id').patch(updateVendorProfile)
+router
+  .route("/updatevendorprofile/:id")
+  .patch(VendorAuthMiddleware, updateVendorProfile);
 
 module.exports = router;
