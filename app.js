a6d579c17ec9790178143154063b294a1c98fca3
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-20 09:22:13 +0300

    kitchen-server

--- app.js
+++ app.js
@@ -26,3 +26,31 @@ app.use(
 app.use(cors());
 
 
+
+
+
+app.get("/wake-up", (req, res) => {
+    res.json({
+      responseType: "success",
+      message: "Server is awake",
+    });
+  });
+
+
+  const DBConnection = async () => {
+    try {
+      await connectionDB(process.env.mongo_url);
+  
+      app.listen(port, () => {
+        console.log(`server is running on port, ${port}`);
+      });
+    } catch (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  };
+  
+  DBConnection();
+
+
+
