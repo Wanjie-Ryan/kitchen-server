b99679d23d48e2f7fb349449d91b07dafb5b01dc
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-20 11:11:08 +0300

    kitchen-server

--- controllers/vendor/reg-log.js
+++ controllers/vendor/reg-log.js
@@ -1,27 +1,78 @@
-const VendorModel = require('../../models/vendor/reg-log')
+const VendorModel = require("../../models/vendor/reg-log");
 const { StatusCodes } = require("http-status-codes");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+const Register = async (req, res) => {
+  try {
+    const { name, email, contact, password } = req.body;
 
-const Register = async(req,res)=>{
+    if (!name || !email || !contact || !password) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "please fill all the fields" });
+    }
+
+    const newVendor = await VendorModel.create(req.body);
+
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "Vendor created successfully" });
+  } catch (err) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, try again later" });
+  }
+};
+
+const Login = async (req, res) => {
 
     try{
 
-        const { name, email, contact, password } = req.body;
+        const { email, password } = req.body;
 
-        if (!name || !email || !contact  || !password) {
-            return res
-              .status(StatusCodes.BAD_REQUEST)
-              .json({ msg: "please fill all the fields" });
-          }
+        if (!email || !password) {
+          return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: "Provide all the fields" });
+        }
 
-          const newVendor =  await VendorModel.create(req.body)
+        const VendorEmail = await VendorModel.findOne({ email });
+
+        if (!VendorEmail) {
+          return res
+            .status(StatusCodes.NOT_FOUND)
+            .json({ msg: "The Email Provided cannot be found" });
+        }
+
+        const correctPassword = await VendorEmail.checkpwd(password);
+
+        if (!correctPassword) {
+          return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ msg: "Incorrect password" });
+        }
+
+        const vendorLogin = VendorEmail.toObject()
+        delete vendorLogin.password
+        delete vendorLogin.email
+        delete vendorLogin.contact
+
+        const token = jwt.sign(
+            { vendorId: vendorLogin._id },
+            process.env.vendor_sec_key,
+            { expiresIn: "1d" }
+          );
 
           return res
-          .status(StatusCodes.CREATED)
-          .json({ msg: "Vendor created successfully" });
+      .status(StatusCodes.OK)
+      .json({ msg: `Login Successful`, vendorLogin, vendorToken: token });
 
+
+
+
+
+    
 
 
     }
@@ -29,16 +80,10 @@ const Register = async(req,res)=>{
 
         res
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ msg: "Something went wrong, try again later" });
+        .json({ msg: "Something went wrong, please try again later" });
     }
 
     
+};
 
-}
-
-const Login = async(req,res)=>{
-
-    res.send('login')
-}
-
-module.exports = {Register, Login}
+module.exports = { Register, Login };
