137f88b1e02495dd6a40f1977a4e9e44340bc204
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-21 12:03:57 +0300

    vendor logics

--- controllers/vendor/products.js
+++ controllers/vendor/products.js
@@ -1,53 +1,44 @@
-const ProductsModel = require('../../models/vendor/products')
-const VendorModel = require('../../models/vendor/reg-log')
+const ProductsModel = require("../../models/vendor/products");
+const VendorModel = require("../../models/vendor/reg-log");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
+const createProduct = async (req, res) => {
+  try {
+    const { productData } = req.body;
 
-const createProduct = async(req,res)=>{
+    const token = req.headers.authorization.split(" ")[1];
 
-    try{
-
-        const {productData} = req.body
-
-        const token = req.headers.authorization.split(" ")[1];
-
-        if (!token) {
-            return res
-              .status(StatusCodes.UNAUTHORIZED)
-              .json({ msg: "You are not authorized to create a product" });
-          }
-
-          const decodedToken = jwt.verify(token, process.env.vendor_sec_key);
-          const vendorId = decodedToken.vendorId;
-
-          const OneVendor =  await VendorModel.findById(vendorId)
-
-          if(!OneVendor){
-            return res.status(StatusCodes.NOT_FOUND).json({msg:'Vendor does not exist,cannot proceed'})
-          }
-
-          const newProduct = await ProductsModel.create({
-            createdBy:OneVendor._id,
-            ...productData
-          })
-
-          return res.status(StatusCodes.CREATED).json({msg:'The product was created successfully', newProduct})
-
-
-
-
+    if (!token) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ msg: "You are not authorized to create a product" });
     }
 
-    catch(err){
+    const decodedToken = jwt.verify(token, process.env.vendor_sec_key);
+    const vendorId = decodedToken.vendorId;
 
-        res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ msg: "Something went wrong, please try again later" });
-    
+    const OneVendor = await VendorModel.findById(vendorId);
 
-
+    if (!OneVendor) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "Vendor does not exist,cannot proceed" });
     }
-}
 
-module.exports = {createProduct}
+    const newProduct = await ProductsModel.create({
+      createdBy: OneVendor._id,
+      ...productData,
+    });
+
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "The product was created successfully", newProduct });
+  } catch (err) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
+
+module.exports = { createProduct };
