94d6f5f0992bf2ab7859844411e9139e48a0c042
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-23 06:17:52 +0300

    user logics

--- routes/user/products.js
+++ routes/user/products.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {GetAllProducts} = require('../../controllers/user/products')
+const {PublicAccess} = require('../../middleware/usermw')
 
-router.route('/userproducts').get(GetAllProducts)
+router.route('/userproducts').get(PublicAccess,GetAllProducts)
 
 
 module.exports = router
