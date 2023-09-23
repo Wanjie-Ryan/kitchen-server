6ff0cb68e8509f9ae70118afe25c4c5a92d71134
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-23 06:28:49 +0300

    user logics

--- controllers/user/products.js
+++ controllers/user/products.js
@@ -1,29 +1,24 @@
-const ProductsModel = require('../../models/vendor/products')
-const { StatusCodes } = require('http-status-codes')
+const ProductsModel = require("../../models/vendor/products");
+const { StatusCodes } = require("http-status-codes");
 
+const GetAllProducts = async (req, res) => {
+  try {
+    const userProducts = await ProductsModel.find({});
 
-
-const GetAllProducts = async(req,res)=>{
-
-    try{
-
-        const userProducts = await ProductsModel.find({})
-
-        if(userProducts.length ===0){
-            return res.status(StatusCodes.NOT_FOUND).json({msg: "No products found"})
-        }
-
-        return res.status(StatusCodes.OK).json({msg:'Products found are:',userProducts})
-
-
-    }
-    catch(err){
-
-        res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ msg: "Something went wrong, please try again later" });
+    if (userProducts.length === 0) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "No products found" });
     }
 
-}
+    return res
+      .status(StatusCodes.OK)
+      .json({ msg: "Products found are:", userProducts });
+  } catch (err) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+  }
+};
 
-module.exports = {GetAllProducts}
+module.exports = { GetAllProducts };
