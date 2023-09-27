7ead6a08987d5c6fdacd9593cebbc61d6666d4be
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-27 08:42:07 +0300

    payments

--- models/payment/pay.js
+++ models/payment/pay.js
@@ -1,34 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-
+const paymentSchema = new mongoose.Schema(
+  {
     Message: {
-        type: String,
-        required: true,
-      },
-      Success: {
-        type: Boolean,
-        required: true,
-      },
-      Status: {
-        type: Number,
-        required: true,
-      },
-      Amount: {
-        type: String,
-      },
+      type: String,
+      required: true,
+    },
+    Success: {
+      type: Boolean,
+      required: true,
+    },
+    Status: {
+      type: Number,
+      required: true,
+    },
+    Amount: {
+      type: String,
+    },
 
-      transaction_reference: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Products",
-        required: true,
-      },
-      transaction_code: {
-        type: String,
-      },
+    transaction_reference: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Products",
+      required: true,
+    },
+    transaction_code: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-
-
-}, {timestamps:true})
-
-module.exports = mongoose.model('Payment',paymentSchema)
+module.exports = mongoose.model("Payment", paymentSchema);
