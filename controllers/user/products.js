574e58f85b7eefcb94efc872b015a7a60e2bfb07
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-29 06:30:02 +0300

    products

--- controllers/user/products.js
+++ controllers/user/products.js
@@ -1,5 +1,6 @@
 const ProductsModel = require("../../models/vendor/products");
 const { StatusCodes } = require("http-status-codes");
+const UserModel = require("../../models/user/reg-log");
 
 const GetAllProducts = async (req, res) => {
   try {
@@ -44,9 +45,56 @@ const GetSingleProduct = async (req, res) => {
   }
 };
 
-const updateProduct = (req,res)=>{
+const updateProduct = async(req,res)=>{
 
-  res.send('hey there')
+  try{
+
+    const {quantity, boughtBy} = req.body
+    const {id:productId} = req.params
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ msg: "You are not authorized" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.user_sec_key);
+    const userId = decodedToken.userId;
+
+    const OneUser = await UserModel.findById(userId);
+
+    if (!OneUser) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "User does not exist" });
+    }
+
+
+    const updateProduct = await ProductsModel.findByIdAndUpdate(productId, {quantity:quantity, boughtBy:OneUser._id}, {new:true})
+
+    if(!updateProduct){
+      return res.status(StatusCodes.NOT_FOUND).json({msg:'Product not found'})
+
+
+    }
+
+    res.status(StatusCodes.OK).json({msg:'Updated Product:',updateProduct});
+
+
+
+
+  }
+  catch(err){
+
+    // console.log(err)
+
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later" });
+
+  }
 
 
 }
